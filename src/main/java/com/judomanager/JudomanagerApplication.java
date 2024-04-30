9c9193b8aa65cd22562b6923460f0038eb4700cf
Ghosttrio <ghosttrio@naver.com>
2024-04-30 15:05:38 +0900

    first commit

---
+++ src/main/java/com/judomanager/JudomanagerApplication.java
@@ -0,0 +1,13 @@
+package com.judomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudomanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudomanagerApplication.class, args);
+	}
+
+}
